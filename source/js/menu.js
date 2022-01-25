eb67986a3ccb084f09e0f6e23ce9280784e84606
Артем Леванов <tema.levanov@gmail.com>
2022-01-25 15:43:04 +0300

    adds search

--- source/js/menu.js
+++ source/js/menu.js
@@ -22,4 +22,69 @@ $(document).ready(function(){
         $('.menu__item').removeClass('active');
         $('.menu__item .menu__sublist').slideUp(400);
     });
+
+    //Работа поиска в шапке
+    const searchButton = $('.menu__search-icon');
+    const searchClose = $('.menu__search-close');
+
+    function openingAndClosingSearch() {
+        const menuList = $('.menu__list');
+        const menuLogin = $('.menu__login');
+        const menuLanguages = $('.menu__languages');
+        const burger = $('.burger');
+        const menuUser = $('.menu__user');
+        const menuForm = $('.menu__search-form');
+        const menuInput = $('.menu__search-input');
+        const menuLeft = $('.menu__left');
+        const header = $('.header');
+
+        menuList.toggleClass('menu__list--search-active');
+        menuLogin.toggleClass('menu__login--search-active');
+        menuLanguages.toggleClass('menu__languages--search-active');
+        burger.toggleClass('burger--search-active');
+        menuUser.toggleClass('menu__user--search-active');
+        menuForm.toggleClass('menu__search-form--search-active');
+        menuLeft.toggleClass('menu__left--search-active');
+        header.toggleClass('header--search-active');
+        menuInput.val('');
+
+    }
+
+    //Работа с поиском по клавише enter
+    $(document).on('keydown', function (e) {
+        const menuInput = $('.menu__search-input');
+        const menuForm = $('.menu__search-form');
+
+        if (e.keyCode === 13) {
+            if (menuInput.val().length > 0) {
+                $(document).off('keydown');
+                menuForm.trigger('submit');
+            } else {
+                openingAndClosingSearch();
+            }
+        }
+    })
+
+    searchButton.on('click', function () {
+        const menuInput = $('.menu__search-input');
+        const menuForm = $('.menu__search-form');
+
+        if (menuInput.val().length > 0) {
+            menuForm.trigger('submit')
+        } else {
+            openingAndClosingSearch();
+        }
+    })
+
+    searchClose.on('click', function () {
+        openingAndClosingSearch();
+    })
+
+    const windowWidth = window.innerWidth
+
+    if (windowWidth < 480) {
+        const menuInput = $('.menu__search-input');
+
+        menuInput.attr("placeholder", "Введите запрос");
+    }
 });
